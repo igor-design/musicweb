041b93d403ec9070325ab2355c5fdf9e06b3cc24
igor-design <isaibra3011@gmail.co>
2020-09-25 16:35:03 +0300

    first commit

---
+++ catalog/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.conf.urls import url
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^music$', views.MusicListView.as_view(), name='music'),
+    url(r'^music/(?P<pk>\d+)$', views.MusicDetailView.as_view(), name='music-detail'),
+]
