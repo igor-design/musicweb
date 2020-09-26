5d00025ee7fc8ae1d5d5db668e86dd197704403f
igor-design <68772217+igor-design@users.noreply.github.com>
2020-09-26 17:03:01 +0300

    Update settings.py

--- musicweb/settings.py
+++ musicweb/settings.py
@@ -122,3 +122,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+
+
+
+
+
